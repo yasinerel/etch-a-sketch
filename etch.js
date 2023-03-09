e4340b73aca6bbb3a076e640ebb088423847d8dd
yasinerel <terel19@ku.edu.tr>
2023-03-09 14:43:56 +0300

    basically done

--- etch.js
+++ etch.js
@@ -1,15 +1,39 @@
-      // Generate the 16x16 grid of square divs
+
+
+
 const container = document.getElementById("grid-container");
-    for (let i = 0; i < 256; i++) {
+
+function createGrid(size) {
+    // Remove existing squares
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    // Create new squares
+    for (let i = 0; i < size * size; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
         container.appendChild(square);
+    }
+
+    // Set grid template columns and rows based on new size
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 }
 
-
-const btn = document.querySelector('.button');
-btn.addEventListener('click', () => {
-    let favDrink = prompt("Number of squares per side for the new grid?");
-  
+// Add event listener to button
+const button = document.querySelector(".button");
+button.addEventListener("click", function() {
+    const size = prompt("Enter number of squares per side:");
+    if(size > 60){
+        alert("enter a number lower than 60");
+    }
+    else if (size !== null && size !== "") {
+        createGrid(parseInt(size));
+    }
 });
+
+// Create initial grid
+createGrid(16);
+
 
